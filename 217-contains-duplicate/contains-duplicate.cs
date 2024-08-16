7a7fd805f5f2c1567be99ba619083044bb1fdb75
Sumit Mitra <mitra.sumit@outlook.com>
2024-08-16 18:30:12 +0530

    Time: 264 ms (67.86%) | Memory: 55.3 MB (92.73%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cs
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -2,9 +2,7 @@ public class Solution {
     public bool ContainsDuplicate(int[] nums) {
         
         HashSet<int> num=new HashSet<int>(nums);
-        
-        if(nums.Length==num.Count)
-            return false;
-        return true;
+            return nums.Length==num.Count?false:true;
+
     }
 }
