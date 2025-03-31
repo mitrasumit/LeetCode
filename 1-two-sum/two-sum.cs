6d58bb6efbfc1ad81b4dae7017a2b6caff902896
Sumit Mitra <mitra.sumit@outlook.com>
2025-04-01 00:28:32 +0530

    Time: 1 ms (98.20%) | Memory: 49 MB (29.32%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -1,15 +1,18 @@
-public class Solution 
-{
-    public int[] TwoSum(int[] nums, int target) 
-    {
-        Dictionary<int,int> n=new Dictionary<int,int>();
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int,int> hashMap=new Dictionary<int,int>();
+
         for(int i=0;i<nums.Length;i++)
         {
-            if(n.TryGetValue(target-nums[i],out int index))
-                return new int[]{i,index};
-        
-        n[nums[i]]=i;
+            var sum=target-nums[i];
+
+            if(hashMap.ContainsKey(sum))
+            {
+                return new[] {i,hashMap[sum]};
+            }
+            if(!hashMap.ContainsKey(nums[i]))
+                hashMap.Add(nums[i],i);
         }
-        return new int[]{};
+        return null;
     }
 }
