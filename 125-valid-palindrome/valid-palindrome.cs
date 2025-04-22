bd4f78ef332b0bab5a083bf9d8e7b6f5d91323c0
Sumit Mitra <mitra.sumit@outlook.com>
2025-04-22 11:53:23 +0530

    Time: 40 ms (19.32%) | Memory: 47.6 MB (22.41%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        
+
+        if(s.Length==1)
+            return true;
+        
+        string pattern = @"[^A-Za-z0-9]";
+         s =  (System.Text.RegularExpressions.Regex.Replace(s, pattern, string.Empty)).ToLower();
+
+        Console.WriteLine(s);
+
+        int right=s.Length-1;
+        int left=0;
+
+        while(left<=right)
+        {
+            if(s[left]==s[right])
+            {
+                left++;
+                right--;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+}
