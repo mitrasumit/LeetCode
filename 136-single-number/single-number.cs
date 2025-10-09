f7500301355400083f0b75d8c209b4cd0ebf11bf
Sumit Mitra <mitra.sumit@outlook.com>
2025-10-09 10:13:08 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (37.17%) - LeetSync

--- 136-single-number/single-number.cs
+++ 136-single-number/single-number.cs
@@ -1,29 +1,14 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-        
-        Dictionary <int,int> dup=new Dictionary<int,int>();
 
-        int value=1;
+     int xor=0;
 
-        for(int i=0;i<nums.Length;i++)
-        {
-            if(dup.ContainsKey(nums[i]))
-            {
-                dup[nums[i]]=value+1;
-            }
-            else
-            {
-                dup.TryAdd(nums[i],value);
-            }
-        }
+     foreach(int num in nums)
+     {
+        xor=xor^num;
+     }
 
-        foreach(var keyValuePair in dup)
-        {
-            if(keyValuePair.Value==1)
-            {
-                return keyValuePair.Key;
-            }
-        }
-        return 0;
+     return xor;
+
     }
 }
