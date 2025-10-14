ba9a9e7d130b9cbda6870b716b55fc9f45d9a657
Sumit Mitra <mitra.sumit@outlook.com>
2025-10-14 13:03:20 +0530

    Time: 1 ms (6.72%) | Memory: 52.4 MB (90.90%) - LeetSync

--- 792-binary-search/binary-search.cs
+++ 792-binary-search/binary-search.cs
@@ -1,25 +1,30 @@
 public class Solution {
     public int Search(int[] nums, int target) {
+        
         int l=0;
         int r=nums.Length-1;
 
         while(l<=r)
         {
-           int m=(l+r)/2;
+            int m=(l+r)/2;
 
             if(target==nums[m])
             {
                 return m;
             }
-            else if(target<nums[m])
-            {
-                r=m-1;
-            }
-            else
+
+            else if(target>nums[m])
             {
                 l=m+1;
             }
+
+            else
+            {
+                r=m-1;
+            }
+            Console.WriteLine(m);
         }
+
         return -1;
     }
 }
